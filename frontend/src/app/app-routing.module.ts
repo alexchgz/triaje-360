7f13e55cdf9c9956cfdda97b86b1d3817b5d3287
Alejandro Chica <alexchgz@gmail.com>
2021-08-02 09:56:53 +0200

    avances frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { RecoveryComponent } from './auth/recovery/recovery.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: AuthLayoutComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: '**', redirectTo: '' }
+  ]},
+
+  { path: 'recovery', component: AuthLayoutComponent,
+    children: [
+      { path: '', component: RecoveryComponent },
+      { path: '**', redirectTo: '' }
+  ]},
+
+  { path: 'dashboard', component: AdminLayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'usuarios', component: UsuariosComponent },
+      { path: '**', redirectTo: '' }
+  ]},
+
+  { path: '**', redirectTo: 'login' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RecoveryComponent } from './auth/recovery/recovery.component';
+import { SidebarComponent } from './commons/sidebar/sidebar.component';
+import { NavbarComponent } from './commons/navbar/navbar.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    AdminLayoutComponent,
+    LoginComponent,
+    RecoveryComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
